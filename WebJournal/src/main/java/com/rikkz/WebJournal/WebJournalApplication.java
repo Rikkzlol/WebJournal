434028c67454e4271fba737214fc3cf9066a33a0
Rikkzlol <sporelok@mail.ru>
2022-09-09 12:16:29 +0300

    Start project

---
+++ WebJournal/src/main/java/com/rikkz/WebJournal/WebJournalApplication.java
@@ -0,0 +1,13 @@
+package com.rikkz.WebJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJournalApplication.class, args);
+	}
+
+}
