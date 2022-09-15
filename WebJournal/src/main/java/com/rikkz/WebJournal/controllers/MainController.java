c1a6ddbc32284f91bcd2d18f27fe59098c730e9b
Rikkzlol <sporelok@mail.ru>
2022-09-15 16:00:53 +0300

    fix some bugs

--- WebJournal/src/main/java/com/rikkz/WebJournal/controllers/MainController.java
+++ WebJournal/src/main/java/com/rikkz/WebJournal/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.rikkz.WebJournal.controllers;
 
+import com.rikkz.WebJournal.models.Post;
+import com.rikkz.WebJournal.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- WebJournal/src/main/java/com/rikkz/WebJournal/models/Post.java
+++ WebJournal/src/main/java/com/rikkz/WebJournal/models/Post.java
@@ -1,57 +1,23 @@
 package com.rikkz.WebJournal.models;
 
+import jdk.jfr.DataAmount;
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String title, anons, fulltext;
+    private String title;
+    private String anons;
+    private String full_text;
     private int views;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAnons() {
-        return anons;
-    }
-
-    public void setAnons(String anons) {
-        this.anons = anons;
-    }
-
-    public String getFulltext() {
-        return fulltext;
-    }
-
-    public void setFulltext(String fulltext) {
-        this.fulltext = fulltext;
-    }
-
-    public int getViews() {
-        return views;
-    }
-
-    public void setViews(int views) {
-        this.views = views;
-    }
 }
