c2e92f59eb29846ca224a365f74fd50a5a36abc8
Rikkzlol <sporelok@mail.ru>
2022-09-16 11:57:06 +0300

    add blogadd site

--- WebJournal/src/main/java/com/rikkz/WebJournal/models/Post.java
+++ WebJournal/src/main/java/com/rikkz/WebJournal/models/Post.java
@@ -20,4 +20,13 @@ public class Post {
     private String anons;
     private String full_text;
     private int views;
+
+    public Post() {
+    }
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
 }
